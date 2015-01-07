956695bff7d01e05c6d67409a8a4bace8d90f84b
kenta.ky.yamamoto <kenta.ky.yamamoto@gree.net>
2015-01-07 16:51:10 +0900

    Added basic game flow

---
+++ abird/Assets/Scripts/Resetter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+    public Rigidbody2D projectile;
+    public float resetSpeed = 0.025f;
+
+    private float resetSpeedSqr;
+    private SpringJoint2D spring;
+
+	void Start () {
+        resetSpeedSqr = resetSpeed * resetSpeed;
+        spring = projectile.GetComponent <SpringJoint2D>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset ();
+        }
+	}
+
+    void OnTriggerExit2D (Collider2D other) {
+        if (other.rigidbody2D == projectile)
+        {
+            Reset ();
+        }
+
+        if (spring == null && projectile.velocity.sqrMagnitude < resetSpeedSqr)
+        {
+            Reset ();
+        }
+    }
+
+    void Reset () {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ abird/Assets/Scripts/TargetDamage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetDamage : MonoBehaviour {
+
+    public int hitPoints = 2;
+    public Sprite damagedSprite;
+    public float damageImpactSpeed;
+
+    private int currentHintPoints;
+    private float damageImpactSpeedSqr;
+    private SpriteRenderer spriteRenderer;
+
+	void Start () {
+        spriteRenderer = GetComponent <SpriteRenderer>();
+        currentHintPoints = hitPoints;
+        damageImpactSpeedSqr = damageImpactSpeed * damageImpactSpeed;
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.collider.tag != "Damager")
+        {
+            return;
+        }
+
+        if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeedSqr)
+        {
+            return;
+        }
+
+        spriteRenderer.sprite = damagedSprite;
+        currentHintPoints --;
+
+        if (currentHintPoints <= 0)
+        {
+            Kill ();
+        }
+	}
+
+    void Kill () {
+        spriteRenderer.enabled = false;
+        collider2D.enabled = false;
+        rigidbody2D.isKinematic = true;
+    }
+}
